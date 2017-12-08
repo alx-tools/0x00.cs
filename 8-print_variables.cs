fd81b464799428812cb579a89117b353295b4227
Carrie Ybay <carrieybay@gmail.com>
2017-12-07 17:44:57 -0800

    Add source code files and README.md

---
+++ 8-print_variables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		 int int1 = 972;
+        	 string str1 = "Mission";
+        	 string str2 = "Street";
+        	 Console.WriteLine("{0} {1} {2}", int1, str1, str2);
+        }
+}
