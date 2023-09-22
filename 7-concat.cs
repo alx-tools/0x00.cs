242c1e6d335fcd6d640ffe2cf506cc8f9505ca75
Guillaume SALVA <gsalva@theroom.com>
2023-09-22 13:14:40 -0700

    Update 7-concat.cs

--- 7-concat.cs
+++ 7-concat.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		string str1 = "Holberton";
 		string str2 = "School";
-		// YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE.
+		// YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE AND DONT MODIFY PREVIOUS LINES
 		Console.WriteLine("Welcome to {0}!", str1);
 	}
 }
